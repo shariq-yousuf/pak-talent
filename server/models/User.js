38154b66757d2671e7906bd9dab4dc96e8890a0c
shariq-yousuf <shariqyousuf01@gmail.com>
2025-05-26 16:30:34 +0500

    feat: implement user creation and deletion functionality and hash password with bcrypt

---
+++ server/models/User.js
@@ -0,0 +1,49 @@
+import mongoose from 'mongoose'
+import { connectToDB } from '../db/db.js'
+const { Schema } = mongoose
+
+const userSchema = new Schema({
+  username: { type: String, required: true },
+  email: { type: String, required: true },
+  passwordHash: { type: String, required: true },
+  companyName: { type: String, required: true },
+  role: {
+    type: String,
+    enum: ['candidate', 'employer', 'admin'],
+    default: 'candidate',
+  },
+})
+
+const User = mongoose.model('User', userSchema)
+
+export const createUser = async (userData) => {
+  try {
+    await connectToDB()
+
+    const user = new User(userData)
+    return await user.save()
+  } catch (error) {
+    console.error('Error creating user:', error?.message)
+    return null
+  }
+}
+
+export const deleteUser = async (userId) => {
+  try {
+    await connectToDB()
+    return await User.findByIdAndDelete(userId)
+  } catch (error) {
+    console.error('Error deleting user:', error?.message)
+    return null
+  }
+}
+
+export const getAllUsers = async () => {
+  try {
+    await connectToDB()
+    return await User.find({})
+  } catch (error) {
+    console.error('Error fetching users:', error?.message)
+    return []
+  }
+}
--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,9 +1,11 @@
 import express from 'express'
 
 import jobsController from '../controllers/jobs.js'
+import usersController from '../controllers/users.js'
 
 const router = express.Router()
 
 router.use('/jobs', jobsController)
+router.use('/users', usersController)
 
 export default router
