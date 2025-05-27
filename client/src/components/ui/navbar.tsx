5d5b19355f50eff6563a3f4f48b1524f2b8ce714
shariq-yousuf <shariqyousuf01@gmail.com>
2025-05-27 15:33:50 +0500

    chore: move navbar to its own component

---
+++ client/src/components/ui/navbar.tsx
@@ -0,0 +1,31 @@
+import useGetUser from '@/hooks/useGetUser'
+import { Link } from 'react-router-dom'
+import { Button } from './button'
+
+const Navbar = () => {
+  const { user } = useGetUser()
+
+  return (
+    <nav className="flex justify-between bg-[#171717] px-6 py-2">
+      <Link to="/" className="text-xl font-semibold text-slate-300 italic">
+        PakHunt
+      </Link>
+
+      {user ? (
+        <Link to={'/profile'}>
+          <Button variant="outline" className="cursor-pointer text-slate-300">
+            Profile
+          </Button>
+        </Link>
+      ) : (
+        <Link to={'/login'}>
+          <Button variant="outline" className="cursor-pointer text-slate-300">
+            Login
+          </Button>
+        </Link>
+      )}
+    </nav>
+  )
+}
+
+export default Navbar
