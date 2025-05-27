6ef8f97d493def9ae50e3c94b5c19540bb0217c1
shariq-yousuf <shariqyousuf01@gmail.com>
2025-05-27 11:17:17 +0500

    chore: conditionally render profile button on nav

--- client/src/pages/layout.tsx
+++ client/src/pages/layout.tsx
@@ -1,7 +1,10 @@
 import { Button } from '@/components/ui/button'
+import useGetUser from '@/hooks/useGetUser'
 import { Link, Outlet } from 'react-router'
 
 const Layout = () => {
+  const user = useGetUser()
+
   return (
     <>
       <nav className="flex justify-between bg-slate-800 px-6 py-2">
@@ -9,11 +12,13 @@ const Layout = () => {
           PakHunt
         </Link>
 
-        <Link to={'/profile'}>
-          <Button variant="outline" className="cursor-pointer text-slate-300">
-            Profile
-          </Button>
-        </Link>
+        {user && (
+          <Link to={'/profile'}>
+            <Button variant="outline" className="cursor-pointer text-slate-300">
+              Profile
+            </Button>
+          </Link>
+        )}
       </nav>
 
       <Outlet />
