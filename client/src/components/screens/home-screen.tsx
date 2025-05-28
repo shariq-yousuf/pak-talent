2bc46165992ce4c6f9fb0853e026d931653bcd89
shariq-yousuf <shariqyousuf01@gmail.com>
2025-05-28 09:19:44 +0500

    fix: improve responsiveness

--- client/src/components/screens/home-screen.tsx
+++ client/src/components/screens/home-screen.tsx
@@ -3,9 +3,9 @@ import { Button } from '../ui/button'
 
 const HomeScreen = () => {
   return (
-    <main className="mx-auto flex min-h-dvh max-w-7xl items-center justify-center p-6">
+    <main className="mx-auto flex min-h-dvh max-w-7xl -translate-y-[50px] items-center justify-center p-4 md:-translate-y-0 md:p-6">
       <div className="flex flex-col items-center gap-4">
-        <h1 className="text-center text-4xl md:text-8xl">Pak Talent</h1>
+        <h1 className="text-center text-5xl md:text-8xl">Pak Talent</h1>
         <p className="max-w-3xl text-center text-lg text-slate-400">
           Pak Talent is a platform that connects job seekers and employers
           across Pakistan. Whether you're looking for your next career
@@ -13,7 +13,7 @@ const HomeScreen = () => {
           the gap with verified listings, and a simple application process.
         </p>
 
-        <div className="mt-4 flex items-center gap-4">
+        <div className="mt-4 flex flex-wrap items-center justify-center gap-4">
           <Link to={'/signup?role=candidate'}>
             <Button className="cursor-pointer text-lg font-semibold">
               Register as Candidate
